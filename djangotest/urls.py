546d8256ab376aee2247c08b47025de4d71c55cc
FfNES <belev90@yandex.ru>
2020-02-17 15:55:47 +0300

    add sqlite, archive

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 from firstapp import views
  
 urlpatterns = [
     path('', views.index),
-    path('dataurl', views.AjaxData)
+    path('dataurl', views.AjaxData),
+    path('dateurl/<str:table_name>', views.DateData)
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,15 +1,27 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 import pickle
- 
+import sqlib
+
 def index(request):
-    return render(request, "index.html")
+	return render(request, "index.html")
 
 def Data():
 	with open("/tmp/some.txt", 'rb') as filehandle:
 		pagesets = pickle.load(filehandle)
 	filehandle.close()
 	return(pagesets)
+
+def DateData(request, table_name):
+	con = sqlib.connection('/opt/sqlite3/***')
+	datedata = {}
+	for i in sqlib.listing_rows(con, table_name):
+		PS = i[1]
+		if datedata.get(PS) and datedata.get(PS).get('data'):
+			datedata[PS] = dict(label = PS, data = (datedata.get(PS).get('data') + [[i[2], i[3]]]))
+		else:
+			datedata[PS] = datedata[PS] = dict(label = PS, data = [[i[2], i[3]]])
+	return JsonResponse(datedata)
 
 def AjaxData(request):
 	return JsonResponse(Data())
